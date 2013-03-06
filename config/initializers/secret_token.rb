3500a08ce75548eeada722a8bd98ea02d5d6b2f1
Peter Lai <peter.yd.lai@gmail.com>
2013-03-06 18:14:14 -0500

    Adding Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasmineDemo::Application.config.secret_token = 'ba3cdd9243d0506a7cff893c35db21909938fd36bb0432e0bd1df51fd399aaaad363bad3dbf6974a1767bdd16cf658e843a9758761fd58c0c9082dc3520c41cc'
