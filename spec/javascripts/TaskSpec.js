6c865651eddccb1ef771f14aae154fc41a1a398f
Dan Garland <dan@dangarland.co.uk>
2013-05-09 22:58:13 -0400

    Adding a mocked out ajax call

--- spec/javascripts/TaskSpec.js
+++ spec/javascripts/TaskSpec.js
@@ -1,30 +1,43 @@
 describe("Tasks", function() {
 
-  describe("when creating a Task", function() {
+  var data;
 
-	it("should give me the newly created Task as JSON", function() {
-	  var callback = jasmine.createSpy();
+  describe("when creating a Task on a real rails server", function() {
+    beforeEach(function() {
+      data = {
+        task : {
+          title: "Learn Jasmine"
+        }
+      };
+    });
 
-	  var data = {
-	    task : {
-	      title: "Learn Jasmine"
-	    }
-	  }
+    it("should give me the newly created Task as JSON", function() {
+      var callback = jasmine.createSpy();
 
-	  createTask(data, callback);
+      createTask(data, callback);
 
       waitsFor(function() {
-        return callback.callCount > 0;
-	  });
+          return callback.callCount > 0;
+      });
 
-	  runs(function() {
-	  	var dataFromRails = callback.mostRecentCall.args[0];
-      expect(dataFromRails.statusText).toNotBe("error");
-	    expect(dataFromRails["title"]).toBe("Learn Jasmine");
-	  });
+      runs(function() {
+      var dataFromRails = callback.mostRecentCall.args[0];
+        expect(dataFromRails.statusText).toNotBe("error");
+        expect(dataFromRails["title"]).toBe("Learn Jasmine");
+      });
 
-	});
+    });
 
+  });
+
+  describe("when calling the dummy ajax method", function() {
+    it("should call the correct URL", function() {
+      spyOn($, "ajax");
+      createTask(data);
+      var lastRequest = $.ajax.mostRecentCall.args[0];
+      expect(lastRequest["url"]).toEqual("http://localhost:3000/tasks");
+      expect(lastRequest.data.task.title).toEqual("Learn Jasmine");
+    });
   });
 
 });
